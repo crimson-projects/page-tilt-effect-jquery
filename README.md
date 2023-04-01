9fbfd22969f87e448d5e2ff0668fa85f218331d3
Jumayev Ubaydullo <82611064+python019@users.noreply.github.com>
2023-04-01 13:27:03 +0500

    base files

---
+++ README.md
@@ -0,0 +1,9 @@
+<div align="center">
+
+# Page Tilt Effect. Jquery | Crimson
+
+<img src="admin/base.png">
+
+### by <a href="https://github.com/python019">SUBUX</a>
+
+</div>
---
+++ js/main.js
@@ -0,0 +1,54 @@
+var paperMenu = {
+	$window: $('#paper-window'),
+	$paperFront: $('#paper-front'),
+	$hamburger: $('.hamburger'),
+	offset: 1800,
+	pageHeight: $('#paper-front').outerHeight(),
+	
+	open: function() {
+		this.$window.addClass('tilt');
+		this.$hamburger.off('click');
+		$('#container, .hamburger').on('click', this.close.bind(this));
+		this.hamburgerFix(true);
+		console.log('opening...');
+	},
+	close: function() {
+		this.$window.removeClass('tilt'); 
+		$('#container, .hamburger').off('click');
+		this.$hamburger.on('click', this.open.bind(this));
+		this.hamburgerFix(false);
+		console.log('closing...');
+	},
+	updateTransformOrigin: function() {
+		scrollTop = this.$window.scrollTop();
+		equation = (scrollTop + this.offset) / this.pageHeight * 100;
+		this.$paperFront.css('transform-origin', 'center ' + equation + '%');
+	},
+	//hamburger icon fix to keep its position
+	hamburgerFix: function(opening) {
+			if(opening) {
+				$('.hamburger').css({
+					position: 'absolute',
+					top: this.$window.scrollTop() + 30 + 'px'
+				});
+			} else {
+				setTimeout(function() {
+					$('.hamburger').css({
+						position: 'fixed',
+						top: '30px'
+					});
+				}, 300);
+			}
+		},
+	bindEvents: function() {
+		this.$hamburger.on('click', this.open.bind(this));
+		$('.close').on('click', this.close.bind(this));
+		this.$window.on('scroll', this.updateTransformOrigin.bind(this));
+	},
+	init: function() {
+		this.bindEvents();
+		this.updateTransformOrigin();
+	},
+};
+
+paperMenu.init();
